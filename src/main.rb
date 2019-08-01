59ff31788935b5480485035fc5ff4894b707b33b
Ian Kottman <ikottman@users.noreply.github.com>
2019-08-01 15:11:50 -0500

    Try out servo pwm

--- src/main.rb
+++ src/main.rb
@@ -2,21 +2,41 @@ require 'faraday'
 require 'json'
 require 'rpi_gpio'
 
-account_id = ENV['NEW_RELIC_ACCOUNT_ID']
-query = ENV['INSIGHTS_QUERY']
-headers =  { 
-  'X-Query-Key' => ENV['NEW_RELIC_INSIGHTS_QUERY_KEY'],
-  'Accept' => 'application/json'
-}
-url = "https://insights-api.newrelic.com/v1/accounts/#{account_id}/query?nrql=#{query}"
 
-response = Faraday.get(url, nil, headers)
-puts response.body
+def call_new_relic()
+  account_id = ENV['NEW_RELIC_ACCOUNT_ID']
+  query = ENV['INSIGHTS_QUERY']
+  headers =  { 
+    'X-Query-Key' => ENV['NEW_RELIC_INSIGHTS_QUERY_KEY'],
+    'Accept' => 'application/json'
+  }
+  url = "https://insights-api.newrelic.com/v1/accounts/#{account_id}/query?nrql=#{query}"
+
+  response = Faraday.get(url, nil, headers)
+  puts response.body
+end
+
+def flash_led()
+  PIN_NUM = 03
+  RPi::GPIO.set_numbering :board
+  RPi::GPIO.setup PIN_NUM, :as => :output
+  RPi::GPIO.set_high PIN_NUM
+  sleep(5)
+  RPi::GPIO.set_low PIN_NUM
+  sleep(5)
+end
+
+def set_angle(pwm, angle):
+  duty = angle / 18 + 2
+  pwm.duty_cycle = duty
+  sleep(3)
+  pwm.duty_cycle = 0
+end
 
 PIN_NUM = 03
-RPi::GPIO.set_numbering :board
-RPi::GPIO.setup PIN_NUM, :as => :output
-RPi::GPIO.set_high PIN_NUM
-sleep(5)
-RPi::GPIO.set_low PIN_NUM
-sleep(5)
+PWM_FREQ = 50
+pwm = RPi::GPIO::PWM.new(PIN_NUM, PWM_FREQ)
+pwm.start(0)
+set_angle(30)
+pwm.stop
+RPi::GPIO.clean_up
